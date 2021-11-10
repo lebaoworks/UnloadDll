e45745639a9a1d967f549b4cdb0d8619ff25f472
lebaoworks <lebaoworks@gmail.com>
2021-11-11 01:23:24 +0700

    Update gullible.cpp

--- gullible/gullible.cpp
+++ gullible/gullible.cpp
@@ -6,23 +6,6 @@
 
 using namespace std;
 
-string GetLastErrorAsString()
-{
-    DWORD errorMessageID = GetLastError();
-    if (errorMessageID == 0) {
-        printf("Error non!\n");
-        return string("");
-    }
-
-    LPSTR messageBuffer = NULL;
-    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-        NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-
-    string message(messageBuffer, size);
-    LocalFree(messageBuffer);
-    return message;
-}
-
 void test_loadlib(const char* name)
 {
     HMODULE hModule = LoadLibraryA(name);
@@ -36,7 +19,6 @@ void test_loadlib(const char* name)
 int main()
 {
     test_loadlib("suspect.dll");
-    HANDLE hThread;
     while (true)
     {
         printf("main is still here!\n");
